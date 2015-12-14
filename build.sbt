4e94d7ef1d9cfe21d92bc042cf4095c856a08ce6
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2015-12-14 12:11:24 +0900

    bug fix

--- build.sbt
+++ build.sbt
@@ -4,8 +4,10 @@ version := "1.0"
 
 scalaVersion := "2.10.6"
 
+scalacOptions ++= Seq("-encoding", "UTF-8")
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.0.0",
-  "org.apache.spark" %% "spark-mllib"  % "1.0.0"
+  "org.apache.spark" %% "spark-core" % "1.5.2",
+  "org.apache.spark" %% "spark-mllib"  % "1.5.2"
 )
+
--- src/main/scala/MLibGBTsDemo.scala
+++ src/main/scala/MLibGBTsDemo.scala
@@ -1,12 +1,18 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.mllib.util.MLUtils
 
 
-object main extends App {
+object MLibGBTsDemo extends App {
+
+  val inputFile = "data/mlib/heart_scale.txt"
+
+  val sc = new SparkContext("local", "demo")
 
   // Load and parse the data file.
-  val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+  val data = MLUtils.loadLibSVMFile(sc, inputFile)
   // Split the data into training and test sets (30% held out for testing)
   val splits = data.randomSplit(Array(0.7, 0.3))
   val (trainingData, testData) = (splits(0), splits(1))
@@ -15,10 +21,12 @@ object main extends App {
   //  Empty categoricalFeaturesInfo indicates all features are continuous.
   val numClasses = 2
   val categoricalFeaturesInfo = Map[Int, Int]()
-  val numTrees = 3 // Use more in practice.
-  val featureSubsetStrategy = "auto" // Let the algorithm choose.
+  val numTrees = 3
+  // Use more in practice.
+  val featureSubsetStrategy = "auto"
+  // Let the algorithm choose.
   val impurity = "gini"
-  val maxDepth = 4
+  val maxDepth = 1
   val maxBins = 32
 
   val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
@@ -29,13 +37,12 @@ object main extends App {
     val prediction = model.predict(point.features)
     (point.label, prediction)
   }
-  val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+  val testErr = labelAndPreds.filter(r => r._1 != r._2).count().toDouble / testData.count()
   println("Test Error = " + testErr)
   println("Learned classification forest model:\n" + model.toDebugString)
 
   // Save and load model
   model.save(sc, "myModelPath")
   val sameModel = RandomForestModel.load(sc, "myModelPath")
-
 
 }
